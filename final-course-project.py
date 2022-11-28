2f3c5b709e6c384d4d8f89e7468b564e56667f57
Marc Rios <marc.rios1712@gmail.com>
2022-11-27 23:00:38 -0800

    build: finding and replacing NaNs

--- final-course-project.py
+++ final-course-project.py
@@ -18,3 +18,18 @@ churn_df = churn_df.round({'MonthlyCharge': 2, 'Tenure': 2, 'Outage_sec_perweek'
 churn_df.drop(columns=churn_df.columns[0], axis=1, inplace=True)
 # print(churn_df)
 
+churn_df['Children'].fillna(0, inplace=True) # removing the NAs in column - in favor of 0
+# print(churn_df['Children'])
+# print(churn_df['Children'].isna().sum())
+print(churn_df.isna().sum()) # find the number in NaNs in column
+
+# churn_df.isnull().sum(axis=1) # Find the number of NaNs in each row
+
+# Replace the NaNs in the column with the median
+# churn_df['Age'].fillna(churn_df['Age'].median(), inplace=True)
+
+# Replace the NaNs in the column with the mean
+# churn_df['Age'].fillna(churn_df['Age'].mean(), inplace=True)
+
+# print(churn_df.dtypes) # Shows the data type of each column. Object means the column has multiple valuet ypes
+
