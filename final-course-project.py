9d27ea78db0ad68872452c5d5a39a0759a353172
Marc Rios <marc.rios1712@gmail.com>
2022-11-29 23:28:49 -0800

    build: detect outliers graphs

--- final-course-project.py
+++ final-course-project.py
@@ -6,25 +6,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-raw_dataset = pd.read_csv('raw_datasets\churn_raw_data.csv')
+raw_dataset = pd.read_csv('raw_datasets\churn_raw_data.csv') # imports csv into a data frame
 
-churn_df = raw_dataset
+churn_df = raw_dataset # the working dataframe 
 
-# ----------------------------- Duplicates -------------------------------
+## ----- Detect Duplicates -----
 # Checks if 1st col equal to 2nd col in data set
 # print(churn_df.iloc[:, 0].equals(churn_df.iloc[:, 1])) # returns True
 
-# remove 1st column; it is a duplicate of the 2nd column
+
+## ----- Treat Duplicates -----
+# Remove 1st column; it is a duplicate of the 2nd column
 churn_df.drop(columns=churn_df.columns[0], axis=1, inplace=True)
 # print(churn_df.head())
 
-# -------------------------- Missing Values ------------------------------
-### have to change all nan values in the columns listed below to a mean or median value
-### we do this for the outlier test. if we dont use mean or median, then the histogram
-### could show outliers, therefore messsing up the entire outlier test
+churn_df = churn_df.round({'MonthlyCharge': 2, 'Tenure': 2, 'Outage_sec_perweek': 1})
 
+
+## ----- Detect Missing Values -----
 # print(churn_df.isna().sum()) # find the number in NaNs in column
 
+
+## ----- Treat Missing Values -----
 # Replace the NaNs in the Age column with the mean
 # ['Children', 'Age', 'Income', 'Techie', 'Phone', 'TechSupport', 'Tenure', 'Bandwidth_GB_Year']
 replace_nan_list = ['Children', 'Age', 'Income', 'Tenure', 'Bandwidth_GB_Year']
@@ -32,49 +35,45 @@ replace_nan_list = ['Children', 'Age', 'Income', 'Tenure', 'Bandwidth_GB_Year']
 for var in replace_nan_list:
     var_dataframe = churn_df[var]
     var_mean = var_dataframe.mean()
-    var_dataframe.fillna(var_mean, inplace=True)
+    var_dataframe.fillna(var_mean, inplace=True) # replaces NaN vals with mean vals
 
-# print(churn_df.isna().sum()) # finds the number in NaNs in column
+## print(churn_df.isna().sum()) # finds the number in NaNs in column
 
-# ----------------------------- Outliers ---------------------------------
-# do for all columns of numeric value
-churn_df['Zscore_Population'] = stats.zscore(churn_df.iloc[::, 0])
-# print(churn_df[['Zscore_Population', 'Population']].head())
+
+## ----- Detect Outliers -----
+# churn_df['Zscore_Population'] = stats.zscore(churn_df.iloc[::, 0])
 # print(churn_df['Zscore_Population'])
 # Plot histogram to check for outliers with Seaborn
-# sns.set()
+sns.set()
+col_header_list = ['Lat', 'Lng', 'Population', 'Children', 'Age', 'Income', 'Outage_sec_perweek',
+                   'Email', 'Contacts', 'Yearly_equip_failure', 'Tenure', 'MonthlyCharge',
+                   'Bandwidth_GB_Year', 'item1', 'item2', 'item3','item4', 'item5', 'item6',
+                   'item7', 'item8']
 
 # col_header_list = ['Lat', 'Lng', 'Population', 'Children', 'Age', 'Income', 'Outage_sec_perweek',
-#                    'Email', 'Contacts', 'Yearly_equip_failure', 'Tenure', 'MonthlyCharge',
-#                    'Bandwidth_GB_Year', 'item1', 'item2', 'item3','item4', 'item5', 'item6',
-#                    'item7', 'item8']
+#                    'Email']
+has_outliers_arr = []
 
-# for header in col_header_list:
-#     z_scores = stats.zscore(churn_df[header])
-#     print(header, type(z_scores))
+for header in col_header_list:
+    z_scores = stats.zscore(churn_df[header])
+    # print(header, type(z_scores))
+    if z_scores.min() < -3.9 or z_scores.max() > 3.9:
+        has_outliers_arr.append(( header, True ))
+    else:
+        has_outliers_arr.append(( header,False ))
 
-#     _ = plt.hist(z_scores)
-#     # _ = plt.hist(churn_df['Population'])
-#     _ = plt.xlabel('X')
-#     _ = plt.ylabel('Y')
-#     plt.show()
+    _ = plt.hist(z_scores)
+    _ = plt.xlabel('Z-Score')
+    _ = plt.ylabel(f'{header} Freq')
+    # plt.show()
+print(has_outliers_arr)
 
-# This is a plot graph
-# zscore_series = churn_df['Zscore_Population']
-# plt.plot(zscore_series)
-# plt.show()
-
-# churn_df['Zscore_Population'].plot(kind='bar')
-# churn_df[['Zscore_Population', 'Population']].plot()
+## ----- Treat Outliers -----
 
 
-# ---------------------- Other data quality issues -----------------------
 
+## ----- Other data quality issues -----
 # limit decimal numbers to 100th point aka 2 decimal nums after point
 #   - MonthlyCharge, Tenure, Outage_sec_perweek, 
-churn_df = churn_df.round({'MonthlyCharge': 2, 'Tenure': 2, 'Outage_sec_perweek': 1})
+# churn_df = churn_df.round({'MonthlyCharge': 2, 'Tenure': 2, 'Outage_sec_perweek': 1})
 # print(churn_df[['MonthlyCharge', 'Tenure', 'Outage_sec_perweek']])
-
-
-# print(churn_df['Age'].min())
-# print(churn_df['Age'].max())
